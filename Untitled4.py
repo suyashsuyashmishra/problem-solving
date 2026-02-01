6a25427451cbf7be241c7dd83026770f9226716a
Suyash <suyash.suyash.mishra@gmail.com>
2026-02-01 20:11:12 +0530

    BANANA GAME

---
+++ Untitled4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# https://www.hackerrank.com/challenges/the-minion-game/problem?isFullScreen=true
+
+# In[ ]:
+
+
+def minion_game(string):
+    vowels = "AEIOU"
+    kevin_score = 0
+    stuart_score = 0
+    n = len(string)
+
+    for i in range(n):
+        if string[i] in vowels:
+            kevin_score += n - i
+        else:
+            stuart_score += n - i
+
+    if kevin_score > stuart_score:
+        print("Kevin", kevin_score)
+    elif stuart_score > kevin_score:
+        print("Stuart", stuart_score)
+    else:
+        print("Draw")
+
